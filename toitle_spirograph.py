9532384d9b5f5e03384191f24c9884dcd9d09e2e
shayanbhattacharya <100789722+shayanbhattacharya@users.noreply.github.com>
2024-03-04 20:28:07 +0000

    Add files via upload
    Using turtle to draw a spirograph.

---
+++ toitle_spirograph.py
@@ -0,0 +1,26 @@
+import turtle as t
+import random
+
+timmy_the_turtle = t.Turtle()
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+timmy_the_turtle.speed("fastest")
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        timmy_the_turtle.color(random_color())
+        timmy_the_turtle.circle(100)
+        timmy_the_turtle.setheading(timmy_the_turtle.heading() + size_of_gap)
+
+
+draw_spirograph(5)
+
+screen = t.Screen()
+screen.exitonclick()
